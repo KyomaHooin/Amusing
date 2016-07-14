dd995e52284b8caa7ea03c05ed0cbafcc49d3974
KyomaHooin <richard_bruna@nm.cz>
2016-07-14 21:14:08 +0200

    Code update.

--- raspberry/rpi-amusing.py
+++ raspberry/rpi-amusing.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
 #
-# Get serial data and send them gziped over HTTP..
+# Transport GZIP serial data over HTTP.
 #
-# Data structure per sensor[AVR]:
+# RAMDISK:
 #
-#               'Got msg #311 : *ZF#T273H370L000B467' - 35 byte string + 2 byte CRLF[37 byte payload]
-#               'Got msg #312 : *ZF#T273H370L000B467'
-#                                .  .   .   .__ Serial number
+# mount -t tmpfs -o size=32m tmpfs /root/amusing/ramdisk
+#
+# DATA:
+#
+#               'Got msg #311 : *ZF#T273H370L000B467'
+#                                .  .   .   .__ Serial numberor longint timestamp??
 #                                .  .   .______ Humidity
 #                                .  .__________ Temperature
 #                                ._____________ Local ID
-#
-# TODO: All processing in RAM..
 #
 
 import httplib
@@ -22,58 +23,72 @@ import gzip
 import os
 import re
 
+LOCATION='archa'
+RAMDISK='/root/amusing/ramdisk/'
 SENSOR_FILE='/root/amusing/rpi-sensor.txt'
-PAYLOAD=37
 
 try:
-
-	try:# [IOError]
-#		#LOG
-		log = open('rpi-amusing.log','a',0)#non-buffering
-		log.write('Program start: ' + time.strftime("%d.%m.%Y %H:%M") + '\n')
-#		#SERIAL BUFFER
-		sensors = {}
-		with open(SENSOR_FILE,'r') as sfile:
-			for line in sfile:
-       				(key, value) = line.split(';')
-      		 		sensors[key] = value
+	try:
+		# DIR
+		os.mkdir(RAMDISK + 'archive')
+		os.mkdir(RAMDISK + 'http')
+	except OSError: pass
+	
+	try:
+		# LOG
+		LOG = open(RAMDISK + 'rpi-amusing.log','a',0)# 0 -> non-buffering
+		LOG.write('Program start: ' + time.strftime("%d.%m.%Y %H:%M") + '\n')
+		#GZIP
+		#CSV=LOCATION + '-' + time.strftime("%Y%m%dT%H%M%S") + '.csv.gz'
+	       	#GZ=gzip.open(RAMDISK + 'http/' + CSV, 'ab')
+		GZ=gzip.open(RAMDISK + 'http/' + LOCATION + '-' + time.strftime("%Y%m%dT%H%M%S") + '.csv.gz', 'ab')
 	except IOError:
-		print 'Failed to open data stream.'
+		print 'Failed to create stream.'
 		exit(1)
 
-	try:# [OSError]
-		#DIR
-		os.mkdir('archive')
-		os.mkdir('http')
-	except OSError:
-		pass
-
 #	MAIN
-
 	while 1:
-
 #		#SERIAL
-
-		s = serial.Serial('/dev/ttyUSB0',9600)# 8,N,1 [default]
-		data=s.read(2 * len(sensors) * PAYLOAD)# blocking.. bytes are doubled!
+		s = serial.Serial('/dev/ttyUSB0',9600,timeout=1)# 8,N,1 [default]
+		data = s.readline()
+		if data != '':
+			print data, time.strftime("%H%M")
+			if re.match('^Got',data):# filter rubbish..
+				#print re.sub('^.* \*([A-Z]{2})#T(\d\d)(\d)H(\d\d)(\d)(.*)$',
+				#	'\\6\;foo', data),
+				print re.sub('^.* \*([A-Z]{2})#T(\d\d)(\d)H(\d\d)(\d)(.*)$',
+					'\\6' + ';temperature;\\2.\\3;' + time.strftime("%Y%m%dT%H%M%S"), data),
+				print re.sub('^.* \*([A-Z]{2})#T(\d\d)(\d)H(\d\d)(\d)(.*)$',
+					'\\6;humidity;\\4.\\5;' + time.strftime("%Y%m%dT%H%M%S"), data),
+				#GZ.write(re.sub('^.* \*([A-Z]{2})#T(\d\d)(\d)H(\d\d)(\d)(.*)$',),
+				#	'\\6;temperature;\\2.\\3;' + time.strftime("%Y%m%dT%H%M%S"), data))
+				#GZ.write(re.sub('^.* \*([A-Z]{2})#T(\d\d)(\d)H(\d\d)(\d)(.*)$',),
+				#	'\\6;humidity;\\4.\\5;' + time.strftime("%Y%m%dT%H%M%S"), data))
 		s.close()
-		log.write(data)
-		log.write(time.strftime("%H:%M") + "\n")
 
-#		#RE(CSV)
+		print 'Nothing..'
 
-#		if re.match('^Got',string):
-# 	        print re.sub('.* \*([A-Z]{2})#T(\d\d)(\d)H(\d\d)(\d).*$','\\1;temperature;\\2.\\3;humidity;\\4.\\5', string)
+#       	gzip.open(RAMDISK + 'http/' + f, 'wb').write(data)
+#		gz=open(RAMDISK + 'http/' + f,'rb')
 
-#		#GZIP[IOError]
+#		if time.strftime("%M") == '45': #every  hour send the pack..
+#			try:
+#				HEADER={'Content-type':'application/octet-stream',
+#					'X-Location':LOCATION + time.strftime("%Y%m%dT%H%M%S")}
+#				c=httplib.HTTPConnection('amusing.nm.cz', '80', timeout=10)
+#				c.request('POST', 'http://amusing.nm.cz/sensors/rawpost.php', gz, header)
+#				r=c.getresponse()
+#				if (r.status == 200):
+#					print "ok"
+#				gz.close()
+#				c.close()
+#			except socket.error:
+#				LOG.write('Socket error HTTP transport failed.')
+		#ARCHIVE
+		#os.rename('http/' + f,'archive/' + f)
 
-#		f=location + '-' + time.strftime("%Y%m%dT%H%M%S") + '.csv.gz'
-#       	gzip.open('http/' + f, 'wb').write(data)
-#		gz=open('http/' + f,'rb')
+		#HTTP[socket.error]
 
-#		#HTTP[socket.error]
-
-#		c=httplib.HTTPConnection('amusing.nm.cz', '80', timeout=10)
 #		c.request('POST', '[removed]', gz, header)
 #		r=c.getresponse()
 #		if (r.status == 200):
@@ -83,6 +98,5 @@ try:
 except Exception as e:
 	print e.args[0]
 	exit(99)
-#archiving..
-#os.rename('http/' + f,'archive/' + f)
-#log.write('Program end.\n')
+LOG.write('Program end.\n')
+LOG.close()
