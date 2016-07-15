63f6f4188c065212308a6c037663aae388d6e9a7
KyomaHooin <richard_bruna@nm.cz>
2016-07-15 11:28:28 +0200

    Logic fixes.

--- raspberry/rpi-amusing.py
+++ raspberry/rpi-amusing.py
@@ -7,7 +7,7 @@
 # tmpfs	/root/amusing/ramdisk	tmpfs	nodev,nosuid,size=32M	0	0
 # mount -t tmpfs -o size=32m tmpfs /root/amusing/ramdisk
 #
-# DATA[10min]:
+# DATA[5min]:
 #
 #               'Got msg #311 : *ZF#T273H370L000B467'
 #                                .  .   .   .__ Serial/timestamp[?]
@@ -24,10 +24,9 @@ import gzip
 import os
 import re
 
-PAYLOAD=''
 LOCATION='archa'
+PAYLOAD=''
 RAMDISK='/root/amusing/ramdisk/'
-SENSOR_FILE='/root/amusing/rpi-sensor.txt'
 
 try:
 	try:	# DIR
@@ -35,35 +34,41 @@ try:
 		os.mkdir(RAMDISK + 'http')
 	except OSError: pass
 	try:	# LOG
-		LOG = open(RAMDISK + 'rpi-amusing.log','a',0)# 0 -> non-buffering
+		LOG = open(RAMDISK + 'rpi-amusing.log','a',0)# non-buffering
 		LOG.write('Program start: ' + time.strftime("%d.%m.%Y %H:%M") + '\n')
 	except IOError:
 		print 'Fail to create log file.'
 		exit(1)
 	while 1:# SERIAL
-		#try:
-		s = serial.Serial('/dev/ttyUSB0',9600,timeout=1)# 8,N,1 [default]
-		data = s.readline()
-		if data != '':
-			print data,# time.strftime("%H%M")
-			pattern = re.compile('^.* \*.(.)#T(\d\d)(\d)H(\d\d)(\d)(.*)$')
-			if re.match(pattern, data):# filter rubbish..
-				print re.sub(pattern,'\\1;temperature;\\2.\\3;' + time.strftime("%Y%m%dT%H%M%S"), data),
-				print re.sub(pattern,'\\1;humidity;\\4.\\5;' + time.strftime("%Y%m%dT%H%M%S"), data),
-				PAYLOAD+=(re.sub(pattern,'\\1;temperature;\\2.\\3;'
-					+ time.strftime("%Y%m%dT%H%M%S") + '\n', data)
-					+ re.sub(pattern,'\\1;humidity;\\4.\\5;'
-					+ time.strftime("%Y%m%dT%H%M%S") + '\n', data))
-		s.close()
-		#except:
-		if time.strftime("%M") == '45': #every hour send the pack..
-			try:	# GZIP
-				GZIP_FILE=RAMDISK + 'http/' + LOCATION + '-' + time.strftime("%Y%m%dT%H%M%S") + '.csv.gz'
-				gzip.open(GZIP_FILE, 'ab').write(PAYLOAD)
-				GZ=open(GZIP_FILE, 'rb')
-			except IOError:
-				LOG.write('Fail to gzip payload.')
-				LOG.close()
+		try:
+			s = serial.Serial('/dev/ttyUSB0',9600,timeout=5)# 8,N,1; 5s scan..
+			data = s.readline()
+			if data != '':
+				print data, 
+				pattern = re.compile('^.* \*.(.)#T(\d\d)(\d)H(\d\d)(\d)(.*)$')
+				if re.match(pattern, data):# rubbish..
+					print re.sub(pattern,'\\1;temperature;\\2.\\3;'
+						+ time.strftime("%Y%m%dT%H%M%S"), data),
+					print re.sub(pattern,'\\1;humidity;\\4.\\5;'
+						+ time.strftime("%Y%m%dT%H%M%S"), data),
+					PAYLOAD+=(re.sub(pattern,'\\1;temperature;\\2.\\3;'
+						+ time.strftime("%Y%m%dT%H%M%S"), data)
+						+ re.sub(pattern,'\\1;humidity;\\4.\\5;'
+						+ time.strftime("%Y%m%dT%H%M%S"), data))
+			s.close()
+		except serial.SerialException:
+			LOG.write('Serial error.')
+			pass
+#		if time.strftime("%M") == '20': #hourly..
+#			try:	# GZIP
+#				GZIP_FILE=RAMDISK + 'http/' + LOCATION + '-' + time.strftime("%Y%m%dT%H%M%S") + '.csv.gz'
+#				gzip.open(GZIP_FILE, 'ab').write(PAYLOAD)
+#				GZ=open(GZIP_FILE, 'rb')
+#				print('Payload ready..')
+#			except IOError:
+#				LOG.write('Fail to gzip payload.')
+#				pass
+#		#for pack in os.listdir(RAMDISK + http):
 #			try:	# HTTP
 #				HEADER={'Content-type':'application/octet-stream',
 #					'X-Location':LOCATION + '-' + time.strftime("%Y%m%dT%H%M%S")}
@@ -71,16 +76,19 @@ try:
 #				c.request('POST', 'http://amusing.nm.cz/sensors/rawpost.php', GZ, HEADER)
 #				r=c.getresponse()
 #				if (r.status == 200):
-#					print "ok"
+#					print "Ok!"
 #				c.close()
 #			except socket.error:
 #				LOG.write('Socket error HTTP transport failed.')
+#				GZ.close()
+#				pass
 #			try:	# CLEANUP
-			GZ.close()
+#				GZ.close()
 #				os.rename('http/' + GZIP_FILE,'archive/' + GZIP_FILE)
-			PAYLOAD=''
-#			except IOError:
-#				LOG.write('Fail to archive payload.')
+#				PAYLOAD=''
+#			except OSError:
+#				print('Allready sent..')# pass ..
+#				#LOG.write('Fail to archive payload.')
 except Exception as e:
 	print e.args[0]
 	exit(99)
