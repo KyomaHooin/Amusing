e34c076b35dc06ccd86d72408756a7c7ffcab949
KyomaHooin <richard_bruna@nm.cz>
2016-11-09 18:17:24 +0100

    Logging, minor fix.

--- report/report.py
+++ report/report.py
@@ -14,49 +14,57 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 
-fbuff = ibuff = StringIO.StringIO()
+ibuff = StringIO.StringIO()# file buffer instance..
+fbuff = StringIO.StringIO()
+
+runtime = time.strftime("%d.%m.%Y %H:%M")
+logfile = '/var/log/report.log'
 
 report= {'[removed]':('archa','42_1-42_2-43_1-43_2-44_1-44_2-45_1-45_2')}
 
-#
+try:# MAIN
+	log = open(logfile,'a')
+except:
+	print('Failed to open log file.')
+	sys.exit(4)
 
 for email in report.keys():
 	try:# GRAPH
-		c = httplib.HTTPSConnection('[removed]','443')
-		c.request('GET','/muzeum/getplotref/' + report[email][1] + '/1D/1/0/0/0/0')
-		r = c.getresponse()
+		conn = httplib.HTTPSConnection('[removed]','443',timeout=15)
+		conn.request('GET','/muzeum/getplotref/' + report[email][1] + '/1D/1/0/0/0/0')
+		r = conn.getresponse()
 		if r.status == 200:
 			ibuff.write(r.read())
 			ibuff.seek(0)# return goddamnit..
+		conn.close()
 	except:
-		print("Failed to get graph.")
+		log.write("Failed to get graph. " + runtime + "\n")
 		sys.exit(3)
-
 	try:# PDF
 		pdf = Canvas(fbuff,pagesize=pagesizes.landscape(pagesizes.A4))
+		pdf.setTitle("Amusing Report 1.3")
 		pdf.setFont('Helvetica', 10)
-		pdf.drawString(50,550,"Amusing Report 1.2")
-		pdf.drawString(640,550,"vygenerováno v " + time.strftime("%d.%m.%Y %H:%M"))
+		pdf.drawString(50,550,"Amusing Report 1.3")
+		pdf.drawString(640,550,"vygenerováno v " + runtime)
 		pdf.line(50,545,790,543)
 		pdf.drawImage(ImageReader(ibuff),70,50,700,470)
 		pdf.line(50,45,790,43)
 		pdf.showPage()
 		pdf.save()
 	except:
-		print('Failed to create PDF.')
+		log.write("Failed to create PDF. " + runtime + "\n")
 		sys.exit(2)
-
 	try:# MAIL
 		text = "\n\nDobrý den,\n\nAutomatický report pro lokalitu: " + \
 			report[email][0] + \
 			"\n\nAmusing NM\n\n------\n\n" + \
 			"Tato zpráva je generována bez možnosti příjmu Vaší odpovědi.\n" + \
-			"Pro odhlášení napište na adresu: [removed] "
+			"Pro odhlášení napište na adresu: richard_bruna@nm.cz"
 
 		msg = MIMEMultipart()
 		msg['From'] = 'Amusing Report <[removed]>'
 		msg['To'] = email
-		msg['Subject'] = "Amusing Report - " + time.strftime("%d.%m.%Y %H:%M")
+		msg['Subject'] = "Amusing Report - " + runtime
 		msg.attach(MIMEText(text,'plain','utf-8'))
 
 		att = MIMEApplication(fbuff.getvalue())
@@ -69,10 +77,12 @@ for email in report.keys():
 		
 		msg.attach(att)
 
-		s = smtplib.SMTP('[removed]')
-		s.sendmail('[removed]', email, msg.as_string())
+		mail = smtplib.SMTP('[removed]')
+		mail.sendmail('[removed]', email, msg.as_string())
+		mail.quit()
 	except:
-		print ('Failed to send email.')
+		log.write("Failed to send email." + runtime + "\n")
 		sys.exit(1)
 fbuff.close()
 ibuff.close()
+
