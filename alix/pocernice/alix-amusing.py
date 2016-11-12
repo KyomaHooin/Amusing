6cb6cbb86c13fcecf10ca0761a1f763f93595480
KyomaHooin <richard_bruna@nm.cz>
2016-11-12 18:05:57 +0100

    Channel handling.

--- alix/pocernice/alix-amusing.py
+++ alix/pocernice/alix-amusing.py
@@ -2,20 +2,26 @@
 #
 # LabJack U3 USB ADC 8 channel
 #
-# DATA:
-#
-#CH01 value = 0.793
-#CH02 value = 0.728
-#CH08 value = 0.899
-#....
+# DATA: CH01 value = 0.793
 #
 
-import httplib,serial,socket,time,gzip,sys,os,re
+import httplib,socket,time,gzip,sys,os,re
 
 PAYLOAD=''
 RAMDISK='/root/amusing/ramdisk/'
-U3='/usr/local/bin/getvalue8'
+U3='/usr/local/bin/getvalues8 > /tmp/u3 2>/dev/null'
+CHAN={	'01':'pocernice_07;humidity',
+	'02':'pocernice_09;humidity',
+	'03':'pocernice_07;temperature',
+	'04':'pocernice_11;temperature',
+	'05':'pocernice_10;humidity',
+	'06':'pocernice_11;humidity',
+	'07':'pocernice_08;humidity',
+	'08':'pocernice_09;temperature'}
+TOKEN=True
 CALL=True
+
+#
 
 try:
 	try:	# DIR
@@ -29,31 +35,27 @@ try:
 		print 'Failed to create log file.'
 		sys.exit(1)
 	while 1:
-		try:	# SERIAL
-			s = serial.Serial('/dev/ttyS0',9600,timeout=5)# 8,N,1; 5s scan..
-			data = s.readline()
-			if data != '':
-				pattern = re.compile('^.* \*.(.)#T(\d\d)(\d)H(\d\d)(\d)L(\d\d)(\d)B(\d)(\d\d).*$')
-				if re.match(pattern, data):# rubbish..
-					PAYLOAD+=(re.sub(pattern,'box6_\\1;temperature;\\2.\\3;'
-						+ time.strftime("%Y%m%dT%H%M%SZ",time.gmtime()), data)
-						+ re.sub(pattern,'box6_\\1;humidity;\\4.\\5;'
-						+ time.strftime("%Y%m%dT%H%M%SZ",time.gmtime()), data)
-						+ re.sub(pattern,'box6_\\1;light;\\6.\\7;'
-						+ time.strftime("%Y%m%dT%H%M%SZ",time.gmtime()), data)
-						+ re.sub(pattern,'box6_\\1;battery;\\8.\\9;'
-						+ time.strftime("%Y%m%dT%H%M%SZ",time.gmtime()), data))
-			s.close()
-			print PAYLOAD
-		except serial.SerialException:
-			LOG.write('Serial error.\n')
-	#	if int(time.strftime("%M")) % 15 == 0 and CALL: # 15 min interval..
-	#		CALL=False
-	#		try:	# GZIP + PAYLOAD
-	#			GZIP_FILE=RAMDISK + 'http/alix-' + time.strftime("%Y%m%dT%H%M%S") + '60.csv.gz'
-	#			gzip.open(GZIP_FILE, 'ab').write(PAYLOAD)
-	#		except IOError:
-	#			LOG.write('Failed to gzip payload.\n')
+		if int(time.strftime("%M")) % 5 == 0 and TOKEN:# 5 min data interval..
+			TOKEN=False
+			cmd = os.system(U3)# LABJACK
+			if cmd == 0:# shell return value
+				try:
+				data = open('/tmp/u3','r').readlines()
+					for line in data:
+						pattern = re.compile('^.*=.(\d).(\d\d\d)$')
+						if re.match(pattern,line):# rubbish..
+							PAYLOAD+=(re.sub(pattern,CHAN[re.sub('^CH(\d\d) .*\n$',"\\1",line)]
+								+ ";\\1.\\2;"
+								+ time.strftime("%Y%m%dT%H%M%SZ",time.gmtime()),line))
+				except IOError:
+					LOG.write('Failed to read U3 data file.' + '\n')
+		if int(time.strftime("%M")) % 15 == 0 and CALL: # 15 min interval..
+			CALL=False
+			try:	# GZIP + PAYLOAD
+				GZIP_FILE=RAMDISK + 'http/alix-' + time.strftime("%Y%m%dT%H%M%S") + '50.csv.gz'
+				gzip.open(GZIP_FILE, 'ab').write(PAYLOAD)
+			except IOError:
+				LOG.write('Failed to gzip payload.\n')
 	#		for PACK in os.listdir(RAMDISK + 'http'):
 	#			try:
 	#				GZIP=open(RAMDISK + 'http/' + PACK, 'rb')
@@ -78,15 +80,17 @@ try:
 	#				LOG.write('Connection error. ' + PACK + '\n')
 	#		# reset buffered payload string..
 	#		PAYLOAD=''
-	#	# reset transport token..
-	#	if int(time.strftime("%M")) % 15 == 1: CALL=True
-	#	# cleanup archive
-	#	for old in os.listdir(RAMDISK + 'archive'):
-	#		if os.path.getmtime(RAMDISK + 'archive/' + old) < (time.time() - 1814400):# 3 week old
-	#			try:
-	#				os.remove(RAMDISK + 'archive/' + old)
-	#			except OSError:
-	#				LOG.write('Failed to remove archive ' + old + '.\n')
+		# reset transport token..
+		if int(time.strftime("%M")) % 15 == 1: CALL=True
+		# cleanup archive
+		for old in os.listdir(RAMDISK + 'archive'):
+			if os.path.getmtime(RAMDISK + 'archive/' + old) < (time.time() - 1814400):# 3 week old
+				try:
+					os.remove(RAMDISK + 'archive/' + old)
+				except OSError:
+					LOG.write('Failed to remove archive ' + old + '.\n')
+		# prevent CPU exhaustion..
+		time.sleep(5)
 except Exception as e:
 	print 'Something bad ' + e.args[0]
 	sys.exit(2)
