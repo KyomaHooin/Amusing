5ee1b8e5463ddae37feb743bd385f5f7ba9adf57
KyomaHooin <richard_bruna@nm.cz>
2016-12-17 20:58:20 +0100

    Syntax.

--- avr/ringo.ino
+++ avr/ringo.ino
@@ -6,7 +6,6 @@
 #include "DHT.h"
 #include <EEPROM.h>
 
-
 // digital pins
 #define DHTPIN         11      // what pin is the DHT connected to
 #define DHTTYPE DHT22         // DHT 11 used
@@ -19,12 +18,7 @@
 #define vSupplyPin   0
 #define vLightPin       1
 
-
-
 DHT dht(DHTPIN, DHTTYPE);
-
-
-
 
 // global parameters
 int     sleepCycles;      // default count of 8 second seep cycles
@@ -34,7 +28,6 @@ int     dhtType;
 
 // ----------------------------------------------------------------
 // ----------------------------------------------------------------
-
 
 //
 // assistance functions
@@ -47,30 +40,21 @@ int     dhtType;
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
 
-
+// ----------------------------------------------------------------
+void activeDHT() {
+  pinMode(DHTPIN, INPUT);
+  digitalWrite(DHTpowerPin,HIGH);
+}
 
 // ----------------------------------------------------------------
-void activeDHT()
- {
-   pinMode(DHTPIN, INPUT);
-   digitalWrite(DHTpowerPin,HIGH);
- };
-
-
+void passiveDHT() {
+  digitalWrite(DHTpowerPin,LOW);
+  pinMode(DHTPIN, OUTPUT);
+  digitalWrite(DHTPIN, LOW);
+}
 
 // ----------------------------------------------------------------
-void passiveDHT()
- {
-   digitalWrite(DHTpowerPin,LOW);
-   pinMode(DHTPIN, OUTPUT);
-   digitalWrite(DHTPIN, LOW);
- };
-
-
-
-// ----------------------------------------------------------------
-void vytuhni8sec()
-{
+void vytuhni8sec() {
   // clear various "reset" flags
   MCUSR = 0;     
   // allow changes, disable reset
@@ -89,18 +73,11 @@ void vytuhni8sec()
   sleep_cpu();  
   
   // cancel sleep as a precaution
-  sleep_disable();
-  
+  sleep_disable();  
 } 
 
-
-
-
- 
 // ----------------------------------------------------------------
-void flash20ms()
- 
-{
+void flash20ms() {
   // be sure the port is output
   pinMode(ledPin,OUTPUT);
 
@@ -110,66 +87,45 @@ void flash20ms()
   
   // turn off flashlight
   digitalWrite(13,LOW);
-
 }
 
-
-
-
-
-
-boolean establishContact() 
-{
+boolean establishContact() {
   int loopCount=0;
-  while (loopCount <= 10)
-   {
-      if(Serial.read() == 's')
-       {
-         return 1;
-       }; 
-      delay(300);
-      loopCount++;
-     };
-    return 0; 
-  
-};
+  while (loopCount <= 10) {
+    if(Serial.read() == 's') { return 1; } 
+    delay(300);
+    loopCount++;
+  }
+  return 0;  
+}
 
-
-
-
-void serialMenu()
-{
-char c;
-while (c != 'g')
- {
-  Serial.println();   
-  Serial.print("SETUP MENU ('h' for help) >>> ");
-  while (Serial.available() == 0) {};
-  c=Serial.read();
-  Serial.println(c);
-  switch (c) 
-     {
+void serialMenu() {
+  char c;
+  while (c != 'g') {
+    Serial.println();   
+    Serial.print("SETUP MENU ('h' for help) >>> ");
+    while (Serial.available() == 0) {};
+    c=Serial.read();
+    Serial.println(c);
+    switch (c) {
       case 'a':
-       editRadioAddress();
-       break;
+        editRadioAddress();
+        break;
       case 'n':
-       editSleepCycles();
-       break;
-/*      case 'd':
-       toggleDHT();
-       break; */
+        editSleepCycles();
+        break;
+/*    case 'd':
+        toggleDHT();
+        break; */
       case 'h':
-       printHelp();
-       break;     
+        printHelp();
+        break;     
 
-     };
- };
-};
+    }
+  }
+}
 
-
-
-void printHelp()
-{
+void printHelp() {
   showTitle();
   Serial.println();
   Serial.println("Commands help:");
@@ -178,101 +134,85 @@ void printHelp()
   Serial.println(" 'd' to toggle DHT sensor type");
   Serial.println(" 'h' to display help & settings");
   Serial.println(" 'g' to start normal operation");
-}; 
+} 
 
+void editRadioAddress() {
+  char c=' ';
+  while (c != 'q') {
+    c = EEPROM.read(10); 
+    Serial.println();   
+    Serial.print("Actual radio address of this unit is [");
+    Serial.print(c);
+    Serial.println("].");
+    Serial.print("Press a key [A..Y] of a new address, or 'q' to quit >>> ");
+    while (Serial.available() == 0) {}; 
+    c=Serial.read();
+    Serial.println(c);
+    if ((c >= 'A') && (c <= 'Y')) {
+      EEPROM.write(10,c);
+      Serial.println("New radio address set.");
+      c='q';
+    } 
+  }
+}
 
-void editRadioAddress()
-{
-char c=' ';
-while (c != 'q')
- {
-  c = EEPROM.read(10); 
-  Serial.println();   
-  Serial.print("Actual radio address of this unit is [");
-  Serial.print(c);
-  Serial.println("].");
-  Serial.print("Press a key [A..Y] of a new address, or 'q' to quit >>> ");
-  while (Serial.available() == 0) {}; 
-  c=Serial.read();
-  Serial.println(c);
-  if ((c >= 'A') && (c <= 'Y'))
-   {
-     EEPROM.write(10,c);
-     Serial.println("New radio address set.");
-     c='q';
-   }; 
- };
-};
+void editSleepCycles() {
+  byte n=0;
+  while (1) {
+    n = EEPROM.read(11); 
+    Serial.println();   
+    Serial.print("Actual number of sleep cycles of this unit is [");
+    Serial.print(n);
+    Serial.println("].");
+    Serial.print("Enter a new value [5..255], or 0 to quit >>> ");
+    while (Serial.available() == 0) {}; 
+    n=Serial.parseInt();
+    Serial.println(n);
+    if ((n >= 5) && (n <= 255)) {
+      EEPROM.write(11,n);
+      Serial.println("New sleep cycle count set.");
+      return;
+    } else { return; }
+  }
+}
 
-
-void editSleepCycles()
-{
-byte n=0;
-while (1)
- {
-  n = EEPROM.read(11); 
-  Serial.println();   
-  Serial.print("Actual number of sleep cycles of this unit is [");
-  Serial.print(n);
-  Serial.println("].");
-  Serial.print("Enter a new value [5..255], or 0 to quit >>> ");
-  while (Serial.available() == 0) {}; 
-  n=Serial.parseInt();
-  Serial.println(n);
-  if ((n >= 5) && (n <= 255))
-   {
-     EEPROM.write(11,n);
-     Serial.println("New sleep cycle count set.");
-     return;
-   }
-  else { return; }; 
- };
-};
-
-
-void toggleDHT()
-{
-char c=' ';
-byte n;
-while (1)
- {
-  n = EEPROM.read(12); 
-  Serial.println();   
-  Serial.print("Actual DHT type setting of this unit is [");
-  if(n == 1)
-   {  Serial.print("DHT11"); }
-  else 
-   {  Serial.print("DHT21"); };
-  Serial.println("].");
-  Serial.print("Press 't' to toggle, 'q' to quit >>> ");
-  while (Serial.available() == 0) {}; 
-  while (Serial.available() == 0) {}; 
-  c=Serial.read();
-  Serial.println(c);
-  switch (c)
-   {
-     case 't':
-       if (n == 0) { n=1; } else { n=0; };
-       EEPROM.write(12,n);
-       Serial.print("New DHT type set [");
-         if(n == 1)
-          {  Serial.print("DHT11"); }
-       else 
-          {  Serial.print("DHT21"); };
-       Serial.println("].");
-       return;
-     break;
-     case 'q':
-       return;
-       break;
-   };  
-  
- };
-};
-
-
-
-
+void toggleDHT() {
+  char c=' ';
+  byte n;
+  while (1) {
+    n = EEPROM.read(12); 
+    Serial.println();   
+    Serial.print("Actual DHT type setting of this unit is [");
+    if(n == 1){
+      Serial.print("DHT11");
+    } else {
+      Serial.print("DHT21");
+    }
+    Serial.println("].");
+    Serial.print("Press 't' to toggle, 'q' to quit >>> ");
+    while (Serial.available() == 0) {}; 
+    while (Serial.available() == 0) {}; 
+    c=Serial.read();
+    Serial.println(c);
+    switch (c) {
+      case 't':
+        if (n == 0) { n=1; } else { n=0; };
+        EEPROM.write(12,n);
+        Serial.print("New DHT type set [");
+        if(n == 1){
+          Serial.print("DHT11");
+        } else {
+          Serial.print("DHT21");
+        }
+        Serial.println("].");
+        return;
+        break;
+      case 'q':
+        return;
+        break;
+    }    
+  }
+}
 
 void showTitle()
 {
@@ -295,31 +235,22 @@ void showTitle()
    {  Serial.print("DHT21"); };
   Serial.println("].");
 */
-};
-
-
-
+}
 
 // ----------------------------------------------------------------
 ISR(WDT_vect) { wdt_disable(); };             // just disable watchdog 
 
-
-
-
-
-void setup() 
- {
+void setup() {
   
   // hardware control pins, modes
   pinMode(radioPowerPin,OUTPUT);
   pinMode(sensorPowerPin,OUTPUT);
   pinMode(DHTpowerPin,OUTPUT);
- 
 
   // set up serial channel 
   Serial.begin(9600);
   // set up VirtualWire radio
-   vw_setup(1000); // Bits per sec
+  vw_setup(1000); // Bits per sec
 
   analogReference(INTERNAL);
 
@@ -333,27 +264,14 @@ void setup()
   showTitle();  
   Serial.println("Press 's' to enter setup mode");
 
-  if (establishContact())
-   {
-     serialMenu();
-   };
-   Serial.println();
-   Serial.println("Resuming the normal operation, power cycle for menu ..."); 
+  if (establishContact()) { serialMenu(); }
+  Serial.println();
+  Serial.println("Resuming the normal operation, power cycle for menu ..."); 
 
-  sleepCycles=EEPROM.read(11);
-  
-  }
+  sleepCycles=EEPROM.read(11);  
+}
 
-
- 
-
-
-
-
-
-void loop() 
- {
- 
+void loop() {
   float vSupp,vLight;
   float hh,tt;
   dht.begin();
@@ -367,12 +285,10 @@ void loop()
   vSupp=analogRead(vSupplyPin)*0.00457;
   vLight=analogRead(vLightPin)*0.00457;
 
-  if (nSends > 0)
-   {
+  if (nSends > 0) {
     hh = dht.readHumidity();
     tt = dht.readTemperature();
-   };
-   
+  }  
   // turn off sensor & DHT power
   digitalWrite(sensorPowerPin,LOW);
   passiveDHT();
@@ -383,8 +299,7 @@ void loop()
   flash20ms();
 
   // Serial printout
-  if (nSends > 0)
-   {
+  if (nSends > 0) {
      Serial.print("Temperature = ");
      Serial.print(tt);
      Serial.println(" *C");
@@ -392,7 +307,7 @@ void loop()
      Serial.print("Humidity = ");
      Serial.print(hh);
      Serial.println(" %");
-   };
+  }
    
   Serial.print("Light = ");
   Serial.print(ll);
@@ -411,7 +326,6 @@ void loop()
 
   flash20ms();
 
-
   // set up data message
   char msg[20];
   int t, p, h, l, b;
@@ -424,14 +338,11 @@ void loop()
 
   // create msg string
   a=EEPROM.read(10);
-  if (nSends > 0)
-   {
+  if (nSends > 0) {
     sprintf(msg, "*Z%c#T%03dH%03dL%03dB%03d", a, t, h, l, b);
-   }
-  else
-   {
-    sprintf(msg, "*Z%c#B%03dU", a, b);
-   };
+  } else {
+     sprintf(msg, "*Z%c#B%03dU", a, b);
+  }
    
   Serial.println(msg);
   Serial.println();
@@ -442,15 +353,14 @@ void loop()
   vw_wait_tx();
   digitalWrite(radioPowerPin,LOW);
 
-
   // calculate the sleeping parameters
-  if (nSends >= 4)
-   { sleepCyclesNow=sleepCycles+random(-2,3); }
-  else
-   { sleepCyclesNow=8 ; nSends++; };
+  if (nSends >= 4) {
+    sleepCyclesNow=sleepCycles+random(-2,3);
+  } else {
+    sleepCyclesNow=8 ; nSends++;
+  }
   Serial.print("Sleeping for ");Serial.print(sleepCyclesNow);Serial.println(" 8-sec periods. G'nite!");Serial.println();
   Serial.flush(); // wait till serial data sent 
-
 
   // stop ADC
   cbi(ADCSRA,ADEN);
@@ -460,25 +370,17 @@ void loop()
   pinMode(A5, OUTPUT);
   digitalWrite(A5, LOW);
 
-
-
   // sleeping with led flashes
-  for (int i=1; i<=sleepCyclesNow; i++)
-   {
-     vytuhni8sec();
-     flash20ms();
-     if (i == (sleepCyclesNow - 1))
-      {
-       activeDHT();
-      }; 
-   };
-
+  for (int i=1; i<=sleepCyclesNow; i++) {
+    vytuhni8sec();
+    flash20ms();
+    if (i == (sleepCyclesNow - 1)) { activeDHT(); } 
+  }
 
   // return I2C pins to default
   pinMode(A4, INPUT);  
   pinMode(A5, INPUT);
   // start ADC
   sbi(ADCSRA,ADEN);
- 
-
 }
+
