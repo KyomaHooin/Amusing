da6b853d9bb0a53019e8bd69816632181883208d
KyomaHooin <richard_bruna@nm.cz>
2016-12-20 11:39:57 +0100

    Revert local to global.

--- avr/george.ino
+++ avr/george.ino
@@ -3,6 +3,7 @@
 //#include <LiquidCrystal.h>
 
 int ledPin = 13;
+int n = 0;// message index
 
 //LiquidCrystal lcd(13, 12, 11, 10, 9, 8);// LCD initialize
 
@@ -26,7 +27,6 @@ void setup() {
 }
 
 void loop() {
-  int n = 0;// message index
   uint8_t buf[VW_MAX_MESSAGE_LEN];
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
   if (vw_get_message(buf, &buflen)) { // Non-blocking
